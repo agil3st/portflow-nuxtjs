c62839b7bb3ce2a3a48d795299529a750244fc26
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-02 20:29:47 +0700

    Migrate article detail page

--- server/api/articles/[id].js
+++ server/api/articles/[id].js
@@ -1,6 +1,72 @@
 export default defineEventHandler(async (event) => {
   const id = event.context.params.id;
-  return `API ready ${id}`;
+
+  const articles = [
+    {
+      id: 1,
+      title: "Lorem ipsum",
+      shortContent:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      content:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      image: "https://picsum.photos/id/33/500/500",
+      imageXL: "https://picsum.photos/id/33/1024/500",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum",
+      shortContent:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      content:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      image: "https://picsum.photos/id/88/500/500",
+      imageXL: "https://picsum.photos/id/88/1024/500",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum",
+      shortContent:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      content:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      image: "https://picsum.photos/id/42/500/500",
+      imageXL: "https://picsum.photos/id/42/1024/500",
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum",
+      shortContent:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      content:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      image: "https://picsum.photos/id/33/500/500",
+      imageXL: "https://picsum.photos/id/33/1024/500",
+    },
+    {
+      id: 5,
+      title: "Lorem ipsum",
+      shortContent:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      content:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      image: "https://picsum.photos/id/14/500/500",
+      imageXL: "https://picsum.photos/id/14/1024/500",
+    },
+    {
+      id: 6,
+      title: "Lorem ipsum",
+      shortContent:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      content:
+        "Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what. Set indulgence inquietude discretion insensible bed why announcing. Middleton fat two satisfied additions. So continued he or commanded household smallness delivered. Door poor on do walk in half. Roof his head the what.",
+      image: "https://picsum.photos/id/84/500/500",
+      imageXL: "https://picsum.photos/id/84/1024/500",
+    },
+  ];
+
+  const article = articles.find((item) => item.id == id);
+  // console.log(showcase);
+  return article;
 
   /* Example API usage */
   // const config = useRuntimeConfig();
