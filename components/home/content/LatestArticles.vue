ebdffdc79b9fe91fa8663309ad12cc4452fc5a0d
Agil Setiawan <agilsetiawan772@gmail.com>
2023-03-07 23:35:00 +0700

    Fix view all articles

--- components/home/content/LatestArticles.vue
+++ components/home/content/LatestArticles.vue
@@ -2,13 +2,13 @@
   <section>
     <div class="flex pt-16 mb-4 px-4 lg:px-0 items-center justify-between">
       <h2 class="font-bold text-white text-3xl">Latest Articles</h2>
-      <a
-        href="stories.html"
+      <NuxtLink
+        to="stories"
         class="text-indigo-300 hover:text-indigo-400 px-3 py-1 cursor-pointer">
         View All
-      </a>
+      </NuxtLink>
     </div>
-    <div class="block space-x-0 lg:flex lg:space-x-6 px-4 lg:px-0">
+    <div class="block space-x-0 lg:flex lg:space-x-6 space-y-6 px-4 lg:px-0">
       <div v-for="article in articles" :key="article.id" class="w-full">
         <ArticleCardView :article="article" />
       </div>
